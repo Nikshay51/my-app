da9d1fd7730e319b717d5bee71c806b093195ff0
Nikshay <nikshay@domain.co.za>
2022-11-14 17:34:25 +0200

    image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import Nikshay from "../assets/nikshay.jpeg";
+
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
-        {/* Container */}
+      {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <div> 
+        <img src= {Nikshay} alt="Profile Image" style={{width: '100px'}}/> 
+       </div>
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
          Nikshay Lalla
