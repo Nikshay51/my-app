1639fe324ce49f926adcd71aeabf95516618db44
Nikshay <nikshay@domain.co.za>
2022-11-14 12:41:30 +0200

    Inital commit- entire application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Navbar/>
+     <Home/>
+     <About/>
+     <Contact/>
     </div>
+    
   );
 }
 
 export default App;
+// Code attribution
+/*
+author:Code commerce
+Date: 10 March 2022
+URL:https://www.youtube.com/watch?v=2kg0z1qNrkw
+github: https://github.com/fireclint/portfolio-app-react
+*/
+/*Github
+https://github.com/machadop1407/react-personal-portfolio-website/tree/master/src/components
+https://www.youtube.com/watch?v=x7mwVn2z3Sk
+https://www.youtube.com/watch?v=hQjlM-8C4Ps*/
+
+
+// Code attribution
+/*
+author:tailwindcss
+Date: No date provided on website
+URL:https://tailwindcss.com/
+*/
+
+// Code attribution
+/*
+author:getform
+Date: no date
+URL:https://getform.io/
+*/
