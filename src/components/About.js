4799bbdfe13f82f5c3e0fa8aca15a2be3782cec1
Nikshay <nikshay@domain.co.za>
2022-11-14 16:20:52 +0200

    changed logo, UI design

--- src/components/About.js
+++ src/components/About.js
@@ -13,36 +13,68 @@ const About = () => {
           <div></div>
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Nikshay Lalla, nice to meet you. Please take a look around.</p>
+            <div >
+               <h1 className='text-20 font-bold border-b-4 inline border-pink-600'> Personal skills: </h1>
+              <li>
+               Organised
+               </li>
+               <li>
+               Leader
+               </li>
+               <li>
+               Team Player
+               </li>
+               <li>
+               Ambitious
+               </li>
+               <li>
+               Empathy
+               </li>
+             
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
+            <div >
+               <h1 className='text-20 font-bold border-b-4 inline border-pink-600'> Development skills: </h1>
+               
+              <p> I have studied BCA - Bachelor in Computer Application Development</p>
+              <li>
+               Technical skills
+               </li>
+               <li>
+                Resourcefulness
+               </li>
+               <li>
+               Problem solving
+               </li>
+               <li>
+               Open mindedness
+               </li>
+               <li>
+               Detail orientated
+               </li>
+             
+            </div> 
             </div>
             <div >
               <p>
-                <h1 className='text-20 font-bold border-b-4 border-pink-600'> Why I want to be a developer at SovTech?</h1>
+                <h1 className='text-20 font-bold border-b-4  border-pink-600'> Why I want to be a developer at SovTech?</h1>
                 I wish to gain insight into the company, understand different departments and what they do, this way I can get a chance to 
-                specialise and with SovTech this is what I want to gain from the programme. 
-                Personal guidance and mentoring, if a senior is assigned to me, I love asking questions to understand every detail and I feel at SovTech this will help me grow and add to my personal growth.
-                A stronger CV, this programme will help me to stand out in the job market as SovTech is a very reputable company.
+                specialise and with SovTech this is what I want to gain from the programme.</p> 
+                <p>Personal guidance and mentoring, if a senior is assigned to me, I love asking questions to understand every detail and I feel at SovTech this will help me grow and add to my personal growth.</p>
+                <p>A stronger CV, this programme will help me to stand out in the job market as SovTech is a very reputable company.</p> 
                 I want to work at SovTech as I have always being intrigued by Software Development and the impact it has had on my life. 
                 I have also found that I achieve more in a collaborative environment and look forward to work in several departments that align to my goals.
-                </p>  
+                  
             </div>
 
             <div>
               <p>
-              <h1 className='text-20 font-bold border-b-4 border-pink-600'> Continued...</h1>
-                I have a passion towards the companies personal mission and feel as an individual with my skill set can grow in the productive environment and add to the mission of the company and it fits well with my core values. 
-                I want to gain exposure in this company and be able to make an impact on the company and gain the trust from my co workers over the time. I want my coworkers to see me as part of the team and someone they can rely on. 
+              
+                I have a passion towards the companies personal mission and feel as an individual with my skill set can grow in the productive environment and add to the mission of the company and it fits well with my core values. </p>
+                I want to gain exposure in this company and be able to make an impact on the company and gain the trust from my co workers over the time. <p>I want my coworkers to see me as part of the team and someone they can rely on. </p>
                 My passion for tech extends beyond IT, it has helped me improve my life and others, I was able to use my skills to big about change "Small wins" graduate programme is my opportunity to be a lump of clay and be moulded into the person I want to be. 
-                I see myself excelling in this role given my experience level, because I have done similar work in the past and know what it takes. 
-                 </p>  
+                <p>I see myself excelling in this role given my experience level, because I have done similar work in the past and know what it takes.</p>
+                   
             </div>
           </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,16 @@ const Home = () => {
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-          NIKSHAY LALLA
+         Nikshay Lalla
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
           I'm a Software Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a Software developer wanting to speacialize in Web development or any form of design, I have created this website to showcase my 
+          <ul>
+          <li className= 'text-center font-bold text-xl text-[#ccd6f6]'>C# developer • Java developer • Web developer </li>
+          </ul>
+          I’m a Software developer wanting to speacialize in Web development and front-end design, I have created this website to showcase my 
           skills and experience. I am pasionate about programming and simply enjoy a challenge, I'm flexible and able to adapt to my environment
         </p>
         <div>
