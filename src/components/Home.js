81f057c46d81ad08cb71aca5e47c82eaaf3f254b
Nikshay <nikshay@domain.co.za>
2023-01-08 19:10:18 +0200

    home and navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
       <div> 
-        <img src= {Nikshay} alt="Profile Image" style={{width: '100px'}}/> 
+        <img src= {Nikshay} alt="Profile Image" style={{width:'150px'}}/> 
        </div>
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,6 +28,16 @@
             About
             </Link>
             </li>
+            <li> 
+            <Link to='skills' smooth={true} duration={500}>
+            Skills
+            </Link>
+          </li>
+          <li> 
+            <Link to='work' smooth={true} duration={500}>
+            Work
+            </Link>
+          </li>
             <li><Link to='contact' smooth={true} duration={500}>
             Contact
             </Link>
@@ -53,10 +63,22 @@
             About
           </Link>
           </li>
+          <li className='py-6 text-4xl'> 
+            <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'> 
+            <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+            </Link>
+          </li>
             <li className='py-6 text-4xl'> 
             <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
             Contact
-          </Link></li>
+          </Link>
+          </li>
+         
            
         </ul>
 
